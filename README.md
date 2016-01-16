10f9f2d0a9a683a932f48c318ffd7f3459b866a8
Tasdik Rahman <tasdik95@gmail.com>
2016-01-16 22:31:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## pygame-boilerplate
+
+A simple boilerplate for getting started with your pygame projects. Umm. Yeah, thats much to it!
+
+A sneak peek.
+
+![Pygame demo](http://i.imgur.com/p7PxMZl.jpg)
+
+The default height is `480px` and width is `360px`
+
+## License
+
+MIT License © [tasdikrahman.me](http://tasdikrahman.me)
+
+A Copy of the License can be found [@http://prodicus.mit-license.org](http://prodicus.mit-license.org)
---
+++ template.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+
+import pygame
+import random
+
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# Define Colors 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+## initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("<Your game>")
+clock = pygame.time.Clock()     ## For syncing the FPS
+
+## Game loop
+running = True
+while running:
+
+    #1 Process input/events
+    clock.tick(FPS)     ## will make the loop run at the same speed all the time
+    for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
+        ## listening for the the X button at the top
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    #2 Update
+    
+
+
+
+    #3 Draw/render
+    screen.fill(BLACK)
+
+    ########################
+
+    ### Your code comes here
+
+    ########################
+
+    ## Done after drawing everything to the screen
+    pygame.display.flip()       
+
+pygame.quit()
